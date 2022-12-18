5816e678f026a9b235a42ebe6b85ae586c78010f
StuffGames <44318828+StuffGames@users.noreply.github.com>
2022-12-18 02:26:18 -0500

    Drag and Drop + About Section Functionality
    JavaScript updated to support for dragging and dropping image files (only png and jpg) into the upload button label on top of also prompting a file select window.
    I separated some repetitive code to make the code look more clean and readable. I created a function that would check the length of the list of files being inputted as to only allow one file. There is also a function that does the procedure for saving the image as a URL and moving to the next step of the process to preview the image.
    
    Move the script tag in the HTML to below the Footer to allow for the script to be able to access footer elements. Now the section with all the "About This Project" text appears when clicked and also disappears when clicked again. For looks I will rotate the arrow whenever the section is open or closed.

--- FruitLens/Javascript/main.js
+++ FruitLens/Javascript/main.js
@@ -2,15 +2,18 @@
 const uploadFruitStartButton = document.querySelector("#image-upload-container-button");
 // Upload Image Button
 const uploadImageInput = document.querySelector("#image-upload");
+// Upload Image Label
+const uploadImageLabel = document.querySelector(".image-upload-label");
 // Image URL Submit Button
 const urlSubmitButton = document.querySelector("#image-url-submit-button");
 // Upload New Image Button
 const newImageButton = document.querySelector("#upload-new-image-button");
 // Identify Fruit Button, moves to next step of the process
 const submitImageButton = document.querySelector("#submit-preview-image-button");
-// About Section Buttton
-const aboutSectionButton = document.getElementsByClassName("about-section-button-class");
-const buttons = document.querySelectorAll('button');
+// About Section Container (contains the paragraphs)
+const aboutSectionContainer = document.querySelector(".about-section-container");
+// About Section Button
+const aboutSectionButton = document.querySelector("#about-section-button");
 
 // Div Conatiner that holds the image uploading step
 const uploadImageContainer = document.querySelector("#upload-image");
@@ -21,7 +24,38 @@ const imagePreviewContainer = document.querySelector("#image-preview");
 let inputImage = null;
 let inputImageURL = '';
 
-console.log(aboutSectionButton)
+// Return a boolean if the input size is equal to 1
+function CheckInputSize(list) {
+    if (list.length === 0) {
+        alert("No Files Inputted");
+        return false;
+    }
+    if (list.length > 1) {
+        alert("Only Input 1 File");
+        return false;
+    }
+    return true;
+}
+
+// Move to Image preview, return true if successful, return false if error occurred
+function AdvanceToImagePreview(file) {
+    const acceptableFiles = ['image/png', 'image/jpg'];
+    inputImage = file;
+
+    if (!acceptableFiles.includes(inputImage.type)) {
+        alert('Please select a .jpg or .png file');
+        return false;
+    }
+    inputImageURL = URL.createObjectURL(inputImage);
+    
+    // Turn off current div and turn on next div
+    uploadImageContainer.style.display = "none";
+    imagePreviewContainer.style.display = "block";
+
+    imagePreviewContainer.children[0].children[0].src = inputImageURL;
+
+    return true;
+}
 
 // Event for "Upload Image of Fruit" button
 // Turns off the display for the button then turns on display for the next div/section
@@ -31,34 +65,37 @@ uploadFruitStartButton.addEventListener('mouseup', () => {
     uploadImageContainer.style.display = "block";
     uploadImageContainer.style.opacity = '1';
     uploadImageContainer.style.visibility = 'visible';
-})
+});
 
 // Event for "Upload Image" Input
 uploadImageInput.addEventListener('change', () => {
     // Check if the input process was done correctly
     const files = uploadImageInput.files;
-    if (files.length === 0) {
-        alert("No Files Inputted");
-        return;
+    if (!CheckInputSize(files.length)) return;
+    const preview = AdvanceToImagePreview(files[0]);
+    if (!preview) return; // Might be unnecessary line
+});
+// Event for "Upload Image" Laebl Drag and Drop Input
+uploadImageLabel.addEventListener('drop', e => {
+    e.preventDefault();
+    
+    if (e.dataTransfer.items) {
+        console.log("Data Transfer Items")
+        if (!CheckInputSize(e.dataTransfer.items)) return;
+        const preview = AdvanceToImagePreview(e.dataTransfer.items[0].getAsFile());
+        if (!preview) return;
     }
-    if (files.length > 1) {
-        alert("Only Input 1 file");
-        return;
+    else {
+        console.log("Data Transfer Files");
+        if (!CheckInputSize(e.dataTransfer.files)) return;
+        const preview = AdvanceToImagePreview(e.dataTransfer.files[0]);
+        if (!preview) return;
     }
-    const acceptableFiles = ['image/png', 'image/jpg'];
-    if (!acceptableFiles.includes(files[0].type)) {
-        alert('Please select a .jpg or .png file');
-        return;
-    }
+});
+uploadImageLabel.addEventListener('dragover', e => {
+    e.preventDefault();
+});
 
-    inputImageURL = URL.createObjectURL(files[0]);
-
-    // Turn off current div and turn on next div
-    uploadImageContainer.style.display = "none";
-    imagePreviewContainer.style.display = "block";
-
-    imagePreviewContainer.children[0].children[0].src = inputImageURL;
-})
 
 // Event for "Upload New Image" Button
 newImageButton.addEventListener('mouseup', () => {
@@ -67,15 +104,23 @@ newImageButton.addEventListener('mouseup', () => {
     // Sets value to empty, making the input have to create a new empty FileList when called
     // This way you can reinput the same image
     uploadImageInput.value = '';
-})
+});
 
 // TODO
-//      Create an even for the submit button, maybe also the input field
+//      Create an event for the submit button, maybe also the input field
 //      See if fetching the image from the URL is successful
 //          If not send an alert and clear the text input
 //          If success then continue to next step with the URL
 //              maybe create a file from the image...
 
-// aboutSectionButton.addEventListener('mouseup', e => {
-//     console.log(e.composedPath());
-// })
+
+// TODO
+//      Make the whole DIV for the About Section Button change color only when hovering over the Button
+//      Possibly add transition to it the same way the button does
+
+// Event For "About Section" Button
+let aboutSectionActive = false;
+aboutSectionButton.addEventListener('mouseup', () => {
+    aboutSectionContainer.style.display = aboutSectionActive ? "none" : "flex";
+    aboutSectionActive = aboutSectionActive ? false : true;
+})
